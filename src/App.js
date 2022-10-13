549064519b0b00890312d8fc85c0a1df58a2aeb2
Barnaan2 <barnaantekalign@gmail.com>
2022-10-12 23:36:22 -0700

    responsive navbar added

--- src/App.js
+++ src/App.js
@@ -19,6 +19,5 @@ const App =()=> {
     </div>
   );
 }
-https://dl3.downloadly.ir/Files/Elearning/Udemy_CSS_Layouts_Masterclass_Build_Responsive-Adaptive_Websites_2022-8.part5_Downloadly.ir.rar
 
 export default App;
--- src/routes/navbar/navbar.route.jsx
+++ src/routes/navbar/navbar.route.jsx
@@ -5,11 +5,11 @@ import './navbar.style.css';
 const Navbar = ()=>{
 return(
     <Fragment>
-<div className="navbar-container">
+<header>
     <div className="logo">
  <img src="https://upload.wikimedia.org/wikipedia/commons/2/22/Portrait_of_Onesimus_Nesib.jpg" alt="Onesimos" />
     </div>
-    <div className="navbar-links">
+    <nav className="navbar-links">
     <Link  className="navbar-link" to="/">
             Home 
         </Link>
@@ -19,8 +19,11 @@ return(
         <Link className="navbar-link" to="/give">
         give
         </Link> 
+    </nav>
+    <div className="form">
+        <input type="search" placeholder="search for church" />
     </div>
-</div>
+</header>
 <Outlet/>
 <Footer/>
     </Fragment>
